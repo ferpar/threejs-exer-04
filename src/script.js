f8747f3c55767de86d8a864f4e65b6066ace2767
Fer <ferp@protonmail.ch>
2024-07-11 10:20:41 +0200

    basic example

--- src/script.js
+++ src/script.js
@@ -12,7 +12,22 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+// Position
+mesh.position.set(0.7, -0.6, 1)
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
+// Rotation
+// reordering is important when you use an external axis to rotate
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
 scene.add(mesh)
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -27,7 +42,10 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+console.log(mesh.position.distanceTo(camera.position))
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
